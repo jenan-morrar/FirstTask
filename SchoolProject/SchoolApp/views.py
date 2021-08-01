1e6153ec153f5d1e2766c59144af4db06df28921
Jenan Morrar <jenanmorrar06@gmail.com>
2021-08-01 19:27:17 +0300

    implementing the project

--- SchoolProject/SchoolApp/views.py
+++ SchoolProject/SchoolApp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect
 from .models import Subject, Student, ClassRoom, Teacher, Grade
+from django.contrib import messages #import messages
 
 
 def index(request):
@@ -51,115 +52,175 @@ def grade(request):
 def studentForm(request):
     subs = Subject.objects.all()
     grds = Grade.objects.all()
-    context = {'subs': subs, 'grds': grds}
+    studentSearch = "Enter Student Name"
+
     if request.method == "POST":
-        if request.POST.get('AddStudent'):
-            # studentInfo = Student.objects.create (studentName=request.POST.get('StudentName'), birthdate=request.POST.get('Birthdate'), studentSubjects=request.POST.get('StudentSubject'), studentGrade=request.POST.get('StudentGrade'))
-            studentGrade = request.POST.get('StudentGrade')
-            studentGradeObject = Grade.objects.get(gradeName=studentGrade)
-            studentInfo = Student.objects.create(studentName=request.POST.get('StudentName'),
-                                                 birthdate=request.POST.get('StudentBirthdate'),
-                                                 studentGrade=studentGradeObject)
-            studentSubject = request.POST.get('StudentSubject')
-            studentSubjectObject = Subject.objects.get(subjectName=studentSubject)
-            studentInfo.studentSubjects.add(studentSubjectObject)
-            # studentInfo.studentGrade.add(studentGradeObject)
-        elif request.POST.get('UpdateStudent'):
+        if request.POST.get('search'):
             studentName = request.POST.get('UpdateStudentName')
-            to_edit = Student.objects.get(studentName=studentName)
-            updatedStudentName = request.POST.get('NewStudentName')
-            updatedStudentBirthdate = request.POST.get('NewStudentBirthdate')
-            #updatedStudentSubject = request.POST.get('NewStudentSubject')
-            #updatedStudentSubjectObject = Subject.objects.get(subjectName=updatedStudentSubject)
-            updatedStudentGrade =request.POST.get('NewStudentGrade')
-            updatedStudentGradeObject = Grade.objects.get(gradeName=updatedStudentGrade)
-            studentSubject = request.POST.get('NewStudentSubject')
-            studentSubjectObject = Subject.objects.get(subjectName=studentSubject)
-            to_edit.studentSubjects.add(studentSubjectObject)
-            to_edit.studentName = updatedStudentName
-            to_edit.birthdate = updatedStudentBirthdate
-            to_edit.studentGrade = updatedStudentGradeObject
-            to_edit.save()
-    return render(request, 'studentForm.html', context)
+            studentSearch = Student.objects.get(studentName=studentName)
+
+        if request.POST.get('AddStudent'):
+            if Student.objects.filter(studentName=request.POST.get('StudentName')).exists():
+                messages.error(request,'The Student already exists!',extra_tags='add')
+            else:
+                studentGrade = request.POST.get('StudentGrade')
+                studentGradeObject = Grade.objects.get(gradeName=studentGrade)
+                studentInfo = Student.objects.create(studentName=request.POST.get('StudentName'),
+                                                     birthdate=request.POST.get('StudentBirthdate'),
+                                                     studentGrade=studentGradeObject)
+                studentSubject = request.POST.get('StudentSubject')
+                studentSubjectObject = Subject.objects.get(subjectName=studentSubject)
+                studentInfo.studentSubjects.add(studentSubjectObject)
+                messages.success(request, 'The Student was successfully added!',extra_tags='add')
+
+        elif request.POST.get('UpdateStudent'):
+            if Student.objects.filter(studentName=request.POST.get('UpdateStudentName')).exists():
+                studentName = request.POST.get('UpdateStudentName')
+                to_edit = Student.objects.get(studentName=studentName)
+                updatedStudentName = request.POST.get('NewStudentName')
+                updatedStudentBirthdate = request.POST.get('NewStudentBirthdate')
+                updatedStudentGrade = request.POST.get('NewStudentGrade')
+                updatedStudentGradeObject = Grade.objects.get(gradeName=updatedStudentGrade)
+                studentSubject = request.POST.get('NewStudentSubject')
+                studentSubjectObject = Subject.objects.get(subjectName=studentSubject)
+                to_edit.studentSubjects.add(studentSubjectObject)
+                to_edit.studentName = updatedStudentName
+                to_edit.birthdate = updatedStudentBirthdate
+                to_edit.studentGrade = updatedStudentGradeObject
+                to_edit.save()
+                messages.success(request, 'The Student was successfully Updated!',extra_tags='add')
+
+            else:
+                messages.error(request,'The Student is not exists!',extra_tags='add')
+
+    return render(request, 'studentForm.html', {'subs': subs, 'grds': grds,'studentSearch':studentSearch})
 
 
 def teacherForm(request):
     subs = Subject.objects.all()
+    teacherSearch = "Enter Teacher Name"
     if request.method == "POST":
-      if request.POST.get('AddTeacher'):
-        # teacherInfo = Teacher.objects.create (teacherName=request.POST.get('TeacherName'), teacherSubjects=request.POST.get('TeacherSubjects'))
-        teacherInfo = Teacher.objects.create(teacherName=request.POST.get('TeacherName'))
-        subject = request.POST.get('TeacherSubjects')
-        subjectObject = Subject.objects.get(subjectName=subject)
-        teacherInfo.teacherSubjects.add(subjectObject)
-      elif request.POST.get('UpdateTeacher'):
+      if request.POST.get('search'):
           teacherName = request.POST.get('UpdateTeacherName')
-          to_edit = Teacher.objects.get(teacherName=teacherName)
-          updatedTeacherName = request.POST.get('NewTeacherName')
-          updatedTeacherSubject = request.POST.get('NewTeacherSubject')
-          updatedTeacherSubjectObject = Subject.objects.get(subjectName=updatedTeacherSubject)
-          to_edit.teacherName = updatedTeacherName
-          to_edit.teacherSubjects.add(updatedTeacherSubjectObject)
-          to_edit.save()
-    return render(request, 'teacherForm.html', {'subs': subs})
+          teacherSearch = Teacher.objects.get(teacherName=teacherName)
+
+      if request.POST.get('AddTeacher'):
+            if Teacher.objects.filter(teacherName=request.POST.get('TeacherName')).exists():
+                messages.error(request,'The Teacher already exists!',extra_tags='add')
+            else:
+                teacherInfo = Teacher.objects.create(teacherName=request.POST.get('TeacherName'))
+                subject = request.POST.get('TeacherSubjects')
+                subjectObject = Subject.objects.get(subjectName=subject)
+                teacherInfo.teacherSubjects.add(subjectObject)
+                messages.success(request, 'The Teacher was successfully added!',extra_tags='add')
+
+      elif request.POST.get('UpdateTeacher'):
+          if Teacher.objects.filter(teacherName=request.POST.get('UpdateTeacherName')).exists():
+              teacherName = request.POST.get('UpdateTeacherName')
+              to_edit = Teacher.objects.get(teacherName=teacherName)
+              updatedTeacherName = request.POST.get('NewTeacherName')
+              updatedTeacherSubject = request.POST.get('NewTeacherSubject')
+              updatedTeacherSubjectObject = Subject.objects.get(subjectName=updatedTeacherSubject)
+              to_edit.teacherName = updatedTeacherName
+              to_edit.teacherSubjects.add(updatedTeacherSubjectObject)
+              to_edit.save()
+              messages.success(request, 'The Teacher was successfully Updated!', extra_tags='add')
+          else:
+              messages.error(request, 'The Teacher is not exists!', extra_tags='add')
+
+    return render(request, 'teacherForm.html', {'subs': subs,'teacherSearch':teacherSearch})
 
 
 def subjectForm(request):
     clsRoom = ClassRoom.objects.all()
+    subjectSearch = "Enter Subject Name"
+
     if request.method == "POST":
+      if request.POST.get('search'):
+          SubjectSearchName = request.POST.get('UpdateSubjectName')
+          subjectSearch = Subject.objects.get(subjectName=SubjectSearchName)
+
       if request.POST.get('AddSubject'):
-        subjectClassRoom = request.POST.get('SubjectClassRoom')
-        subjectClassRoomObject = ClassRoom.objects.get(classRoomName=subjectClassRoom)
-        subjectInfo = Subject.objects.create(subjectName=request.POST.get('SubjectName'),
-                                             classRoomName=subjectClassRoomObject)
+            if Subject.objects.filter(subjectName=request.POST.get('SubjectName')).exists():
+                messages.error(request,'The Subject already exists!',extra_tags='add')
+            else:
+                subjectClassRoom = request.POST.get('SubjectClassRoom')
+                subjectClassRoomObject = ClassRoom.objects.get(classRoomName=subjectClassRoom)
+                subjectInfo = Subject.objects.create(subjectName=request.POST.get('SubjectName'),
+                                                     classRoomName=subjectClassRoomObject)
+                messages.success(request, 'The Subject was successfully added!',extra_tags='add')
+
       elif request.POST.get('UpdateSubject'):
-          SubjectName = request.POST.get('UpdateSubjectName')
-          to_edit = Subject.objects.get(subjectName=SubjectName)
-          updatedSubjectName = request.POST.get('NewSubjectName')
-          updatedSubjectClass = request.POST.get('NewSubjectClassRoom')
-          updatedSubjectClassObject = ClassRoom.objects.get(classRoomName=updatedSubjectClass)
-          to_edit.subjectName = updatedSubjectName
-          to_edit.classRoomName = updatedSubjectClassObject
-          to_edit.save()
+          if Subject.objects.filter(subjectName=request.POST.get('UpdateSubjectName')).exists():
+              SubjectName = request.POST.get('UpdateSubjectName')
+              to_edit = Subject.objects.get(subjectName=SubjectName)
+              updatedSubjectName = request.POST.get('NewSubjectName')
+              updatedSubjectClass = request.POST.get('NewSubjectClassRoom')
+              updatedSubjectClassObject = ClassRoom.objects.get(classRoomName=updatedSubjectClass)
+              to_edit.subjectName = updatedSubjectName
+              to_edit.classRoomName = updatedSubjectClassObject
+              to_edit.save()
+              messages.success(request, 'The Subject was successfully Updated!', extra_tags='add')
 
+          else:
+              messages.error(request, 'The Subject is not exists!', extra_tags='add')
 
-    return render(request, 'subjectForm.html', {'clsRoom': clsRoom})
+    return render(request, 'subjectForm.html', {'clsRoom': clsRoom, 'subjectSearch':subjectSearch})
 
 
 def classRoomForm(request):
+    ClassRoomSearch = 'Enter Room Name'
     if request.method == "POST":
-        if request.POST.get('AddClassRoom'):
-            roomName = ClassRoom.objects.create(classRoomName=request.POST.get('RoomName'))
-        elif request.POST.get('UpdateClassRoom'):
-            className = request.POST.get('UpdateRoomName')
-            to_edit = ClassRoom.objects.get(classRoomName=className)
-            updatedClass = request.POST.get('NewRoomName')
-            to_edit.classRoomName = updatedClass
-            to_edit.save()
+        if request.POST.get('search'):
+            classRoomName = request.POST.get('UpdateRoomName')
+            ClassRoomSearch = ClassRoom.objects.get(classRoomName=classRoomName)
 
-    return render(request, 'classRoomForm.html')
+        if request.POST.get('AddClassRoom'):
+            if ClassRoom.objects.filter(classRoomName=request.POST.get('RoomName')).exists():
+                messages.error(request,'The ClassRoom already exists!',extra_tags='add')
+            else:
+                roomName = ClassRoom.objects.create(classRoomName=request.POST.get('RoomName'))
+                messages.success(request, 'The ClassRoom was successfully added!',extra_tags='add')
+
+        elif request.POST.get('UpdateClassRoom'):
+            if ClassRoom.objects.filter(classRoomName=request.POST.get('UpdateRoomName')).exists():
+                className = request.POST.get('UpdateRoomName')
+                to_edit = ClassRoom.objects.get(classRoomName=className)
+                updatedClass = request.POST.get('NewRoomName')
+                to_edit.classRoomName = updatedClass
+                to_edit.save()
+                messages.success(request, 'The ClassRoom was successfully Updated!',extra_tags='add')
+
+            else:
+                messages.error(request,'The ClassRoom is not exists!',extra_tags='add')
+
+    return render(request, 'classRoomForm.html',{'ClassRoomSearch':ClassRoomSearch})
 
 
 def gradeForm(request):
+    gradeSearch='Enter Grade Name'
     if request.method == "POST":
+        if request.POST.get('search'):
+            gradeSearchName = request.POST.get('UpdateGradeName')
+            gradeSearch = Grade.objects.get(gradeName=gradeSearchName)
+
         if request.POST.get('AddGrade'):
-            gradeName = Grade.objects.create(gradeName=request.POST.get('GradeName'))
+            if Grade.objects.filter(gradeName=request.POST.get('GradeName')).exists():
+                messages.error(request,'The Grade already exists!',extra_tags='add')
+            else:
+               gradeName = Grade.objects.create(gradeName=request.POST.get('GradeName'))
+               messages.success(request, 'The Grade was successfully added!',extra_tags='add')
+
             return render(request, 'gradeForm.html')
         elif request.POST.get('UpdateGrade'):
-            gradeName = request.POST.get('UpdateGradeName')
-            to_edit = Grade.objects.get(gradeName=gradeName)
-            updatedGrade = request.POST.get('NewGradeName')
-            to_edit.gradeName = updatedGrade
-            to_edit.save()
-            """ grade = Grade.objects.get('UpdateGradeName')
-            print(grade)
-            to_update = Grade.objects.filter(gradeName='UpdateGradeName').update(gradeName=request.POST.get('NewGradeName'))"""
+            if Grade.objects.filter(gradeName=request.POST.get('UpdateGradeName')).exists():
+                gradeName = request.POST.get('UpdateGradeName')
+                to_edit = Grade.objects.get(gradeName=gradeName)
+                updatedGrade = request.POST.get('NewGradeName')
+                to_edit.gradeName = updatedGrade
+                to_edit.save()
+                messages.success(request,'The Grade was successfully Updated !',extra_tags='update')
+            else:
+               messages.error(request, 'The Grade is not exists!',extra_tags='update')
 
-            """  grdName = Grade.objects.get(gradeName='UpdateGradeName')
-              form = gradeForms(request.POST,instance=grdName)
-              if form.is_valid():
-                  form.save()
-                  messages.success(request,"Grade Updated Successfully")
-                  return render(request, 'gradeForm.html', {'grdName': grdName})"""
-
-    return render(request, 'gradeForm.html')
+    return render(request, 'gradeForm.html',{'gradeSearch':gradeSearch})
